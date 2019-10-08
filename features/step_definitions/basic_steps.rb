0c2629f57d2daa8ee03cfad0fae75cfc571c7c11
AlexSaxena <saxenaalex@gmail.com>
2019-10-08 14:43:06 +0200

    Adds feature file for sign_up, Adds step def.

---
+++ features/step_definitions/basic_steps.rb
@@ -0,0 +1,17 @@
+Given("I visit the landing page") do
+  visit root_path
+end
+
+Given("I click {string}") do |element|
+  click_on element
+end
+
+When("I fill in {string} with {string}") do |field, content|
+  fill_in field, with: content
+end
+
+Given("the following user exists") do |table|
+  table.hashes.each do |user|
+    FactoryBot.create(:user, user)
+  end
+end
