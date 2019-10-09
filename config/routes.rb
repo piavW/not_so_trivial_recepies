6c3ed36ae196c4921e5c40bdff963b36ee7735fa
AlexSaxena <saxenaalex@gmail.com>
2019-10-09 14:04:10 +0200

    Creates omniauth_callback_controller.rb, Adds omniauth to user.rb, Adds button in applciation.haml, Adds omniauth in routes, Cleans up comments in devise.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root controller: :recipes, action: :index
   resources :recipes, only: [:index]
-  devise_for :users
+  resources :users
+  devise_for :users, controllers: {
+  omniauth_callbacks: :omniauth_callbacks
+}
 end
