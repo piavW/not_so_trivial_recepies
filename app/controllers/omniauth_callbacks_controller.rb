64d54704c991a23bbad3e945dd2c8954881bc6eb
AlexSaxena <saxenaalex@gmail.com>
2019-10-09 15:32:47 +0200

    Corrects typos.

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,10 +1,10 @@
-class OmniauthCallbacksController < Devise::OminauthCallbacksController
+class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
 def facebook
   @user = User.from_omniauth(request.env['omniauth.auth'])
 
   if @user.persisted?
-    sign_in_redirect @user, event: :authentication
+    sign_in_and_redirect @user, event: :authentication
     set_flash_message(:notice, :success, kind: 'Facebook') if is_navigational_format?
   else
     session['devise.facebook_date'] = request.env['ominauth.auth']
