43b888b7df614e27ebecfcbfc9866eb1c2145d9a
AlexSaxena <saxenaalex@gmail.com>
2019-10-10 10:02:04 +0200

    Adds payment resources routes and configures Stripe Api secret keys.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root controller: :recipes, action: :index
   resources :recipes, only: [:index]
+  resources :payments, only: [:new, :create]
   devise_for :users
 end
