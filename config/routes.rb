1193f5b7d0b2ed3d053b8dcc5c36a1dd4cbf3beb
piavw <piavonwachenfelt@hotmail.com>
2019-10-10 10:56:47 +0200

    Updates routes, refactors charges index-file to display payment details.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root controller: :recipes, action: :index
   resources :recipes, only: [:index]
-  resources :charges, only: [:new, :create]
+  resources :charges, only: [:index, :new, :create]
   devise_for :users
 end
