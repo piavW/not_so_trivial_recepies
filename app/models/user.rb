d3601bf176974dcefc2d766533d23f2c496253c3
piavw <piavonwachenfelt@hotmail.com>
2019-10-10 11:19:39 +0200

    Refactors code to fix indentation.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,13 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: [:facebook]
 
-def self.new_with_session(params, session)
-  super.tap do |user|
-    if data = session['devise.facebook_data'] && session['devise.facebook_data']['extra']['raw-info']
-      user.email = data['email'] if user.email.blank?
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session['devise.facebook_data'] && session['devise.facebook_data']['extra']['raw-info']
+        user.email = data['email'] if user.email.blank?
+      end
     end
   end
-end
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
