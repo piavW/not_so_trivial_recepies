0f95cb281ef2ec086b70980829d3be9f41d4713e
yasmine <yasmine.ezequiel@gmail.com>
2019-10-10 10:15:52 +0200

    Initializes stripe services and renames controller and reconfigures routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root controller: :recipes, action: :index
   resources :recipes, only: [:index]
-  resources :payments, only: [:new, :create]
+  resources :charges, only: [:new, :create]
   devise_for :users
 end
