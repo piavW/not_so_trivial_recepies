9654c5f39d1642a905d9dc5c4a24857ea2ee5863
piavw <piavonwachenfelt@hotmail.com>
2019-10-11 11:26:58 +0200

    Begins to implement stripe javascript, updates basic-step, adds form in charges new.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,16 +1,16 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
-// vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file. JavaScript code in this file should be added after the last require_* statement.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
-//= require rails-ujs
-//= require activestorage
-//= require turbolinks
-//= require_tree .
+require("rails-ujs").start()
+require("turbolinks").start()
+
+const initiateStripe = (stripeForm) => {
+  const stripe = Stripe('public_key')
+  const elements = stripe.elements()
+  const card = elements.create('card')
+  card.mount('#card-element')
+}
+
+document.addEventListener('turbolinks:load', () => {
+  let stripeForm = document.getElementById('charge_form')
+  if (stripeForm) {
+    initiateStripe(stripeForm)
+  }
+})
--- features/step_definitions/basic_steps.rb
+++ features/step_definitions/basic_steps.rb
@@ -19,8 +19,15 @@ Given("I click on {string} for {string} membership") do |button_text, membership
   end
 end
 
-Then("I fill in the Stripe field {string} with {string}") do |field, value|
-  fill_in "Field",	with: "value" 
+Then("I fill in the Stripe field {string} with {string}") do |input_field, value|
+  stripe_frame = find("iframe[name='__privateStripeFrame4']")
+  case input_field
+  when "CC Number"
+    field = 'cardnumber'
+  end
+    within_frame(stripe_frame) do
+      find(field).send_keys(value)
+    end
 end
 
 Then("I submit the Stripe form") do
