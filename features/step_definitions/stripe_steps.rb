148de00d39328f128ebedc87cb786006944ad525
piavw <piavonwachenfelt@hotmail.com>
2019-10-12 15:09:29 +0200

    Updates stripe step def with frame.

--- features/step_definitions/stripe_steps.rb
+++ features/step_definitions/stripe_steps.rb
@@ -1,15 +1,22 @@
 
-Then("I fill in the Stripe field {string} with {string}") do |input_field, value|
-  stripe_frame = find("iframe[name='__privateStripeFrame4']")
-  case input_field
-  when "CC Number"
-    field = 'cardnumber'
-  end
-    within_frame(stripe_frame) do
-      find(field).send_keys(value)
-    end
+Then("I submit the Stripe form") do
+  click_on "Submit Payment"
 end
 
-Then("I submit the Stripe form") do
-  click_on submit
+Then("I fill in Stipe field {string} with {string}") do |input_field, value|
+  case input_field
+  when "CC Number"
+    frame_name = '#card-number div iframe'
+    field_name = 'cardnumber'
+    when "Expiry date"
+  frame_name = '#card-expiry div iframe'
+  field_name = 'exp-date'
+    when "CVC"
+  frame_name = '#card-cvc div iframe'
+  field_name = 'cvc'
+  end
+  stripe_frame= find(frame_name)
+  within_frame stripe_frame do
+    page.driver.browser.find_element(name: field_name).send_keys(value)
+  end
 end
